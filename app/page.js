0ae0a8eabc669d4ff7c56394cfe7335c71e36af0
Artem <artem.konovalov.2001@inbox.ru>
2025-05-07 18:41:11 +0300

    added hero section (flex)

--- app/page.js
+++ app/page.js
@@ -2,15 +2,23 @@ export default function Home() {
   return (
     <div>
       <main >
-
-        <div className="py-32 px-8 mx-auto text-center"> 
+        <div className="bg-base-200">
+        <section className="bg-base-200 flex justify-between items-center px-8 py-4 max-w-3xl mx-auto">
+        <div className="font-bold">PsWd</div>
+        <div className="space-x-4 hidden md:block">
+          <a className="link link-hover">Pricing</a>
+          <a className="link link-hover">FAQ</a>
+        </div>
+        </section>
+        </div>
+        <div className="py-32 px-8 mx-auto text-center "> 
             <h1 className="text-4xl font-extrabold mb-6 py-10">Welcome to the Pswd</h1>
-            <input type="text" placeholder="Type here" className="input input-primary size-2/5"/> 
+            <input type="text" placeholder="Type here" className="input input-primary size-2/4"/> 
         </div>
 
         <div className="mx-auto text-center">
 
-        <button className="btn btn-primary">Send</button>
+        <button className="btn bg-slate-300">Send</button>
         </div>
             
 
