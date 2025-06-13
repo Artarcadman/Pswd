236d88f88533daa7ce0414676a4027264a875691
Artem <artem.konovalov.2001@inbox.ru>
2025-06-13 03:55:49 +0300

    gray

--- app/page.js
+++ app/page.js
@@ -21,7 +21,7 @@ export default function Home() {
     <div className="min-h-screen bg-base-200 text-base-content">
       <main>
         {/* Header */}
-        <header className="bg-base-200">
+        <header className="bg-base-300">
           <div className="max-w-4xl mx-auto flex justify-between items-center px-4 sm:px-6 md:px-8 py-4">
             <div className="font-bold text-lg">PsWd</div>
             <nav className="hidden md:flex space-x-6">
@@ -31,22 +31,20 @@ export default function Home() {
         </header>
 
         {/* Hero */}
-        <section className="text-center py-12 px-4 sm:py-16">
-          <h1 className="text-3xl sm:text-4xl md:text-5xl font-extrabold mb-6">
+        <section className="text-center py-12 px-4 sm:py-16 flex flex-col md:flex-row gap-10 justify-center items-center">
+          
+
+          <Image className="w-96 rounded-xl grayscale" src={Demo} alt="Demo"/>
+          
+          
+          <section>
+            <h1 className="text-3xl sm:text-4xl md:text-5xl font-extrabold mb-6 ">
             Welcome to PsWd
-          </h1>
-
-          <Image src={Demo} alt="Demo"/>
-        </section>
+            </h1>
 
 
-        
-
-
-
-        {/* Input Form */}
-        <section className="bg-base-200 py-6 px-4">
-          <div className="max-w-md mx-auto flex flex-col sm:flex-row justify-center items-center gap-4">
+            {/* Input Form */}
+            <div className="max-w-md mx-auto flex flex-col sm:flex-row justify-center items-center gap-4">
             <input
               type="text"
               placeholder="Type here"
@@ -55,24 +53,18 @@ export default function Home() {
               
               onChange = {handleInputChange}
             />
-            
             <button className="btn btn-neutral w-full sm:w-auto">Send</button>
-            
           </div>
-          
-        </section>
 
-        {/* SymbolCounter */}
+          {/* SymbolCounter */}
         <section className="text-center">
 
         <p className="text-error font-bold">
           {counter >= 18 ? "limit reached!" : ""}
         </p>
-          
-          
-      
-
+          </section>
         </section>
+     </section>
         
       </main>
     </div>
